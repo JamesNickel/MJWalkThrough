4887e4330a0f45b477c9030d2f0533e9c405e353
Jamesnickel2 <mrjavadkhodashenas@gmail.com>
2022-02-21 09:07:54 +0330

    Some cleanup

--- app/src/main/java/com/myhome/mjwalkthrough/MainActivity.java
+++ app/src/main/java/com/myhome/mjwalkthrough/MainActivity.java
@@ -1,76 +1,57 @@
 package com.myhome.mjwalkthrough;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.myhome.mjwalkthrough.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.preference.PreferenceManager;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    public static final String KEY_APP_WALK_THROUGH_IS_VISITED = "app_walk_through_is_visited";
+    private TextView txtFirst, txtSecond;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+        txtFirst = findViewById(R.id.txt_first);
+        txtSecond = findViewById(R.id.txt_second);
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+
+        showWalkThrough();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    void showWalkThrough(){
+        boolean dialogVisited = preferences.getBoolean(KEY_APP_WALK_THROUGH_IS_VISITED, false);
+        if(!dialogVisited){
+            List<ItemHollowView> viewList = new ArrayList<>();
+            viewList.add(new ItemHollowView(
+                    txtFirst,
+                    "First title",
+                    "First description"
+            ));
+            viewList.add(new ItemHollowView(
+                    txtSecond,
+                    "Second title",
+                    "Second description"
+            ));
+            new HollowViewManager(this)
+                    .setViewList(viewList)
+                    .setListener(() -> {
+                        //preferences.edit().putBoolean(KEY_APP_WALK_THROUGH_IS_VISITED, true).apply();
+                    })
+                    .start();
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
